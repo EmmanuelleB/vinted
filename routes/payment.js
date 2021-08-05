ea7573d5a9ac202342479bb0dfc2d8aa3c568dd6
Emmanuelle Baron <emmanuellebaron1@gmail.com>
2021-08-05 21:41:37 +0200

    change emplacement of dotenv

--- routes/payment.js
+++ routes/payment.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const isAuthentificated = require("../middlewares/isAuthentificated");
-require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_API_KEY);
 
 router.post("/payment", isAuthentificated, async (req, res) => {
@@ -11,13 +10,6 @@ router.post("/payment", isAuthentificated, async (req, res) => {
     const stripeToken = req.fields.stripeToken;
 
     console.log(stripeToken);
-
-    // const response = await stripe.charges.create({
-    //     amount: 2000,
-    //     currency: "eur",
-    //     description: "La description de l'objet",
-    //     source: stripeToken
-    // })
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
